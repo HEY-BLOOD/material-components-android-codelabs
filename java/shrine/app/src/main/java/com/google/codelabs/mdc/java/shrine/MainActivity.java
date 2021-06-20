755efe594c96336e824e8ccf6e5ec95903ce50a7
blood <hey-blood@foxmail.com>
2021-06-21 00:49:22 +0800

    java: 添加顶部应用栏

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
@@ -1,6 +1,9 @@
 package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -38,5 +41,11 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
         }
 
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.shr_toolbar_menu, menu);
+        return true;
     }
 }
--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 public class ProductGridFragment extends Fragment {
@@ -12,7 +15,24 @@ public class ProductGridFragment extends Fragment {
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+        // Inflate the layout for this fragment with the ProductGrid theme
+        View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+
+        // Set up the toolbar
+        setUpToolbar(view);
+
+        return view;
     }
+
+    /**
+     * 设置工具栏
+     */
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+
 }
